e83437586e444077e3beca0cbec6e336b55f30c5
Adam Lackorzynski <adam@l4re.org>
2017-03-20 00:00:00 +0000

    ARM: Add Renesas R-Car Gen3 M3
    Change-Id: I002e5b3fe3f90ac203c97ddf33bd75d0f5cad66b

---
+++ server/src/platform/rcar3.cc
@@ -0,0 +1,41 @@
+/*!
+ * \file
+ * \brief  Support for Renesas Gen3
+ *
+ * \date   2016-2017
+ * \author Adam Lackorzynski <adam@l4re.org>
+ *
+ */
+/*
+ * (c) 2016-2017 Author(s)
+ *
+ * This file is part of L4Re and distributed under the terms of the
+ * GNU General Public License 2.
+ * Please see the COPYING-GPL-2 file for details.
+ */
+
+#include <l4/drivers/uart_sh.h>
+#include "support.h"
+#include "startup.h"
+
+namespace {
+class Platform_arm_rcar3 : public Platform_single_region_ram
+{
+  bool probe() { return true; }
+
+  void init()
+  {
+    kuart.base_address = 0xe6e88000;
+    kuart.reg_shift    = 0;
+    kuart.base_baud    = 14745600;
+    kuart.baud         = 115200;
+    kuart.irqno        = 196;
+    static L4::Uart_sh _uart;
+    static L4::Io_register_block_mmio r(kuart.base_address);
+    _uart.startup(&r);
+    set_stdio_uart(&_uart);
+  }
+};
+}
+
+REGISTER_PLATFORM(Platform_arm_rcar3);
