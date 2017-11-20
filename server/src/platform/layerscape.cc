4a56e554624154bee18d31683d7efc795b1ca4b8
Adam Lackorzynski <adam@l4re.org>
2017-11-20 00:00:00 +0000

    layerscape: Add IRQs for other UARTs
    Change-Id: I06fb74f6d7cf324aac14e55aa2a41ed4fa9915a4

--- server/src/platform/layerscape.cc
+++ server/src/platform/layerscape.cc
@@ -1,13 +1,13 @@
 /*!
  * \file
- * \brief  Support for Freescale TWR-LS1021A
+ * \brief  Support for NXP/Freescale LS1021A and LS1012A
  *
- * \date   2015
+ * \date   2015-2017
  * \author Adam Lackorzynski <adam@l4re.org>
  *
  */
 /*
- * (c) 2015 Author(s)
+ * (c) 2015-2017 Author(s)
  *
  * This file is part of L4Re and distributed under the terms of the
  * GNU General Public License 2.
@@ -18,7 +18,7 @@
 #include "mmio_16550.h"
 
 namespace {
-class Platform_arm_ls1021atwr : public Platform_single_region_ram
+class Platform_arm_ls10xxa : public Platform_single_region_ram
 {
   bool probe() { return true; }
 
@@ -26,17 +26,27 @@ class Platform_arm_ls1021atwr : public Platform_single_region_ram
   {
     switch (PLATFORM_UART_NR)
       {
+      default:
       case 0:
         kuart.base_address = 0x21c0500;
         break;
       case 1:
         kuart.base_address = 0x21c0600;
         break;
+#if defined(PLATFORM_TYPE_ls1021atwr)
+      case 2:
+        kuart.base_address = 0x21d0500;
+        break;
+      case 3:
+        kuart.base_address = 0x21d0600;
+        break;
+#endif
       };
 
+    // Two UARTs share an IRQ
 #if defined(PLATFORM_TYPE_ls1021atwr)
     kuart.base_baud    = 9375000;
-    kuart.irqno        = 118;
+    kuart.irqno        = 118 + (PLATFORM_UART_NR / 2);
 #elif defined (PLATFORM_TYPE_ls1012afrdm)
     kuart.base_baud    = 7812500;
     kuart.irqno        = 86;
@@ -46,7 +56,7 @@ class Platform_arm_ls1021atwr : public Platform_single_region_ram
 
     kuart.baud         = 115200;
     kuart.reg_shift    = 0;
-    static L4::Uart_16550 _uart(kuart.base_baud, 0, 0, 0, 0);
+    static L4::Uart_16550 _uart(kuart.base_baud);
     setup_16550_mmio_uart(&_uart);
   }
 
@@ -61,4 +71,4 @@ class Platform_arm_ls1021atwr : public Platform_single_region_ram
 };
 }
 
-REGISTER_PLATFORM(Platform_arm_ls1021atwr);
+REGISTER_PLATFORM(Platform_arm_ls10xxa);
