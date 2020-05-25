48f1d76f91bcd303746b97aa4c99e2d99199d7b0
Adam Lackorzynski <adam@l4re.org>
2020-05-25 00:00:00 +0000

    Remove unused macro
    Change-Id: Icc719104be05b69b562e7c566b25950292acad8b

--- server/src/ARCH-amd64/macros.h
+++ server/src/ARCH-amd64/macros.h
@@ -16,6 +16,5 @@
 #define L4_CHAR_PTR(x)		(char*)(l4_addr_t)(x)
 #define L4_CONST_CHAR_PTR(x)	(const char*)(l4_addr_t)(x)
 #define L4_VOID_PTR(x)		(void*)(l4_addr_t)(x)
-#define L4_MB_MOD_PTR(x)	(l4util_mb_mod_t*)(l4_addr_t)(x)
 
 #endif  /* ! __ARCH_AMD64_MACROS_H__ */
--- server/src/ARCH-x86/macros.h
+++ server/src/ARCH-x86/macros.h
@@ -12,6 +12,5 @@
 #define L4_CHAR_PTR		(char*)
 #define L4_CONST_CHAR_PTR	(const char*)
 #define L4_VOID_PTR		(void*)
-#define L4_MB_MOD_PTR		(l4util_mb_mod_t*)
 
 #endif  /* ! __ARCH_X86_MACROS_H__ */
