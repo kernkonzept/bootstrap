f8cbc5a3a510c76e160f26759516097aa902437b
Frank Mehnert <frank.mehnert@kernkonzept.com>
2020-10-12 00:00:00 +0000

    Uart: implement non-blocked writing for all drivers
    Adapt Dual_uart and Uart_vga to the interface change. While at it, add
    the missing 'override' keywords to the virtual UART functions.
    
    Change-Id: I733c537bc9a1b9471c70603802183e53063f838f

--- server/src/platform/x86_pc-base.h
+++ server/src/platform/x86_pc-base.h
@@ -454,7 +454,7 @@ public:
     return c;
   }
 
-  int write(char const *s, unsigned long count) const override
+  int write(char const *s, unsigned long count, bool) const override
   {
     int r = _u1->write(s, count);
     if (_u2)
@@ -773,7 +773,14 @@ public:
     return raw_keyboard_getscancode() != -1;
   }
 
-  int write(char const *s, unsigned long count) const override
+  void out_char(char c) const
+  {
+    if (c == 10)
+      vga_putchar(13);
+    vga_putchar(c);
+  }
+
+  int write(char const *s, unsigned long count, bool) const override
   {
     unsigned long c = count;
     while (c)
