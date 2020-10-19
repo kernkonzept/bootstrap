14c304e62908e6a239fa3d4a54a4a1d3fb3c9ca4
Adam Lackorzynski <adam@l4re.org>
2020-10-19 00:00:00 +0000

    Add Arm NXP i.MX8Q platform
    This is for the i.MX8QM and i.MX8QP platforms.
    
    It auto detects the 4GB/8GB versions.
    
    Change-Id: I60ec5766ca054461fc43628717d0c087ee7f80a2

--- server/src/platform/imx.cc
+++ server/src/platform/imx.cc
@@ -24,7 +24,7 @@
 
 namespace {
 class Platform_arm_imx
-#ifdef PLATFORM_TYPE_imx8x
+#if defined(PLATFORM_TYPE_imx8x) || defined(PLATFORM_TYPE_imx8q)
   : public Platform_base, public Boot_modules_image_mode
 #else
   : public Platform_single_region_ram
@@ -141,20 +141,26 @@ class Platform_arm_imx
     };
     _wdog_phys = 0x30280000;
     static L4::Uart_imx8 _uart;
-#elif defined(PLATFORM_TYPE_imx8x)
+#elif defined(PLATFORM_TYPE_imx8x) || defined(PLATFORM_TYPE_imx8q)
+
+#ifdef PLATFORM_TYPE_imx8x
+    enum { Uart_irq_base = 32 + 225 };
+#else
+    enum { Uart_irq_base = 32 + 345 };
+#endif
     switch (PLATFORM_UART_NR) {
       default:
       case 1: kuart.base_address = 0x5a060000; // LPUART0
-              kuart.irqno        = 32 + 225;
+              kuart.irqno        = Uart_irq_base;
               break;
       case 2: kuart.base_address = 0x5a070000; // LPUART1
-              kuart.irqno        = 32 + 226;
+              kuart.irqno        = Uart_irq_base + 1;
               break;
       case 3: kuart.base_address = 0x5a080000; // LPUART2
-              kuart.irqno        = 32 + 227;
+              kuart.irqno        = Uart_irq_base + 2;
               break;
       case 4: kuart.base_address = 0x5a090000; // LPUART3
-              kuart.irqno        = 32 + 228;
+              kuart.irqno        = Uart_irq_base + 3;
               break;
     };
     static L4::Uart_lpuart _uart;
@@ -166,13 +172,146 @@ class Platform_arm_imx
     set_stdio_uart(&_uart);
   }
 
-#ifdef PLATFORM_TYPE_imx8x
+#if defined(PLATFORM_TYPE_imx8x)
   void setup_memory_map()
   {
     mem_manager->ram->add(Region(0x080200000, 0x083ffffff, ".ram", Region::Ram));
     mem_manager->ram->add(Region(0x088000000, 0x08fffffff, ".ram", Region::Ram));
     mem_manager->ram->add(Region(0x095c00000, 0x0ffffffff, ".ram", Region::Ram));
     mem_manager->ram->add(Region(0x880000000, 0x8bfffffff, ".ram", Region::Ram));
+  }
+
+  Boot_modules *modules() { return this; }
+#elif defined(PLATFORM_TYPE_imx8q)
+  enum
+  {
+    MU              = 0x5d1c0000,
+    MU_SR_TE0_MASK1 = 1 << 23,
+    MU_SR_RF0_MASK1 = 1 << 27,
+    MU_ATR0_OFFSET1 = 0x0,
+    MU_ARR0_OFFSET1 = 0x10,
+    MU_ASR_OFFSET1  = 0x20,
+    MU_ACR_OFFSET1  = 0x24
+  };
+
+  L4::Io_register_block_mmio _mu{MU};
+
+  void write_mu(unsigned int regIndex, l4_uint32_t msg)
+  {
+    unsigned int mask = MU_SR_TE0_MASK1 >> regIndex;
+
+    // Wait for TX to be empty
+    while (!(_mu.read32(MU_ASR_OFFSET1) & mask))
+      ;
+    _mu.write32(MU_ATR0_OFFSET1  + (regIndex * 4), msg);
+  }
+
+  void read_mu(unsigned int regIndex, l4_uint32_t *msg)
+  {
+    unsigned int mask = MU_SR_RF0_MASK1 >> regIndex;
+
+    // Wait for RX to be full
+    while (!(_mu.read32(MU_ASR_OFFSET1) & mask))
+      ;
+    *msg = _mu.read32(MU_ARR0_OFFSET1 + (regIndex * 4));
+  }
+
+  struct Scu_msg
+  {
+    union {
+      struct {
+        l4_uint8_t ver;
+        l4_uint8_t size;
+        l4_uint8_t svc;
+        l4_uint8_t func;
+      } h;
+      l4_uint32_t hdr;
+    };
+    l4_uint32_t data[4];
+
+    enum { Scu_svc_rm = 3 };
+
+    explicit Scu_msg(l4_uint8_t func, l4_uint8_t sz)
+    {
+      h.ver = 1;
+      h.svc = Scu_svc_rm;
+      h.size = sz;
+      h.func = func;
+    }
+  };
+
+  void scu_call(Scu_msg *msg, bool has_result)
+  {
+    // Write header
+    write_mu(0, msg->hdr);
+    l4_uint8_t count = 1;
+
+    // Write data into the 4 channels
+    while (count < msg->h.size)
+      {
+        write_mu(count % 4 , msg->data[count - 1]);
+        count++;
+      }
+
+    if (has_result)
+      {
+        // Read header
+        read_mu(0, &msg->hdr);
+        count = 1;
+
+        // Read data from the 4 channels
+        while (count < msg->h.size)
+          {
+            read_mu(count % 4, &msg->data[count - 1]);
+            count++;
+          }
+      }
+  }
+
+  bool scu_mem_owned(l4_uint8_t mr)
+  {
+    Scu_msg msg(21, 2);
+    msg.data[0] = mr;
+    scu_call(&msg, true);
+    return (l4_uint8_t)msg.data[0];
+  }
+
+  void scu_mem_info(l4_uint8_t mr, l4_uint64_t *s, l4_uint64_t *e)
+  {
+    Scu_msg msg(22, 2);
+    msg.data[0] = mr;
+    scu_call(&msg, true);
+    *s = msg.data[1] | (l4_uint64_t)msg.data[0] << 32;
+    *e = msg.data[3] | (l4_uint64_t)msg.data[2] << 32;
+  }
+
+  void setup_memory_map()
+  {
+    // Find the end of the second dram bank
+    l4_uint64_t end = 0x8ffffffff; // Default to 2GB
+    for (int mr = 0; mr < 64; ++mr)
+      {
+        if (!scu_mem_owned(mr))
+          continue;
+
+        l4_uint64_t a, b;
+        scu_mem_info(mr, &a, &b);
+        if (a == 0x880000000)
+          {
+            if (b == 0xfffffffff)
+              end = 0x97fffffff; // 6GB variant
+            else if (b <= 0x9ffffffff)
+              end = b;
+            // Otherwise leave at default
+
+            break;
+          }
+      }
+
+    mem_manager->ram->add(Region(0x080200000, 0x083ffffff, ".ram", Region::Ram));
+    mem_manager->ram->add(Region(0x088000000, 0x08fffffff, ".ram", Region::Ram));
+    mem_manager->ram->add(Region(0x095c00000, 0x0ffffffff, ".ram", Region::Ram));
+    mem_manager->ram->add(Region(0x880000000, end, ".ram", Region::Ram));
   }
 
   Boot_modules *modules() { return this; }
