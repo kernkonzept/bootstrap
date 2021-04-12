7b4f20de24631ec78761c6c1c41843d89e4c3ec9
Stephan Gerhold <stephan@gerhold.net>
2021-04-12 00:00:00 +0000

    platform: Add initial set of Qualcomm Snapdragon platforms
    These are used in various popular smartphones, tablets, smartwatches,
    laptops, embedded/automotive boards, ...
    
    - Qualcomm Snapdragon 400 (MSM8226)
    - Qualcomm Snapdragon 210 (MSM8909)
    - Qualcomm Snapdragon 410/412 (MSM8916)
    - Qualcomm Snapdragon 615 (MSM8939)
    - Qualcomm Snapdragon 800 (MSM8974)
    - Qualcomm Snapdragon 855 (SM8150)
    
    This initial set just corresponds to those that have been tested
    successfully on some device so far, more platforms could likely
    be added easily with reasonable effort.
    
    Unfortunately it ends up being a quite confusing maze of #if macros to
    handle address differences between all the platforms. There is no good
    way around that, a similar approach is already used in imx.cc for
    example.
    
    In the future it might be possible to also read the UART address from
    the device tree instead. However, this would require some rework in
    bootstrap to parse the device tree much earlier. The disadvantage is
    that it would be no longer possible to have debug console output when
    the DT is missing or invalid.
    
    Change-Id: Ibd06520187afdad4aa5b924512c56230e7a4dff9
    Requires-Change-Id: I795f2d223af558b70c10e098ef7e1b9a5b8aa11a
    Requires-Change-Id: I6b8d8b47e66676d8f8ff276809b271453af4d256

---
+++ server/src/platform/qcom.cc
@@ -0,0 +1,75 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR License-Ref-kk-custom */
+/*
+ * Copyright (C) 2021-2022 Stephan Gerhold <stephan@gerhold.net>
+ * Copyright (C) 2022-2023 Kernkonzept GmbH.
+ */
+
+#include <l4/drivers/uart_dm.h>
+#include <l4/drivers/uart_geni.h>
+#include "dt.h"
+#include "startup.h"
+#include "support.h"
+
+namespace {
+class Platform_arm_qcom : public Platform_dt
+{
+  bool probe() override { return true; }
+
+  void init() override
+  {
+    kuart.baud         = 115200;
+    kuart.access_type  = L4_kernel_options::Uart_type_mmio;
+    kuart_flags       |=   L4_kernel_options::F_uart_base
+                         | L4_kernel_options::F_uart_baud
+                         | L4_kernel_options::F_uart_irq;
+
+#if defined(PLATFORM_TYPE_msm8226)
+    kuart.base_address = 0xf991f000; // UART3
+    kuart.irqno        = 32 + 109;
+    static L4::Uart_dm _uart(kuart.base_baud);
+#elif defined(PLATFORM_TYPE_msm8909)
+    kuart.base_address = 0x078af000; // UART1
+    kuart.irqno        = 32 + 107;
+    static L4::Uart_dm _uart(kuart.base_baud);
+#elif defined(PLATFORM_TYPE_msm8916) || defined(PLATFORM_TYPE_msm8939)
+    kuart.base_address = 0x078b0000; // UART2
+    kuart.irqno        = 32 + 108;
+    static L4::Uart_dm _uart(kuart.base_baud);
+#elif defined(PLATFORM_TYPE_msm8974)
+    kuart.base_address = 0xf991e000; // UART1
+    kuart.irqno        = 32 + 107;
+    static L4::Uartdm _uart(kuart.base_baud);
+#elif defined(PLATFORM_TYPE_sm8150)
+    kuart.base_address = 0x00a90000; // UART2
+    kuart.irqno        = 32 + 357;
+    static L4::Uart_geni _uart(kuart.base_baud);
+#else
+#error Unknown Qualcomm platform type
+#endif
+
+    static L4::Io_register_block_mmio r(kuart.base_address);
+    _uart.startup(&r);
+    set_stdio_uart(&_uart);
+  }
+
+  void reboot() override
+  {
+    reboot_psci();
+
+    // Fallback for older platforms without PSCI support in the firmware
+    if (dt.have_fdt())
+      {
+        l4_uint64_t addr;
+        Dt::Node pshold = dt.node_by_compatible("qcom,pshold");
+        if (pshold.is_valid() && pshold.get_reg(0, &addr))
+          *reinterpret_cast<volatile l4_uint32_t *>(addr) = 0;
+        else
+          printf("PSCI reboot failed and no 'qcom,pshold' address in DT.\n");
+      }
+
+    l4_infinite_loop();
+  }
+};
+}
+
+REGISTER_PLATFORM(Platform_arm_qcom);
