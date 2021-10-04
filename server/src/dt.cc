f67682f2ae0ca6f4a520a02d94c8e42328c720de
Georg Kotheimer <georg.kotheimer@kernkonzept.com>
2021-10-04 00:00:00 +0000

    Introduce read-only wrapper for libfdt
    The wrapper accepts a flattened device tree, usually provided by the
    boot loader, then checks it for validity.
    
    The wrapper offers a user-friendly API for working with the device tree
    and common functionality such as setting up memory regions according to
    the device tree.
    
    Change-Id: I18f74dda7889350a81fd20dd90e0bc4c0b35d301

---
+++ server/src/dt.cc
@@ -0,0 +1,212 @@
+/* SPDX-License-Identifier: GPL-2.0-only or License-Ref-kk-custom */
+/*
+ * Copyright (C) 2021 Kernkonzept GmbH.
+ * Author(s): Georg Kotheimer <georg.kotheimer@kernkonzept.com>
+ */
+
+#include <stdio.h>
+#include <assert.h>
+
+#include "panic.h"
+#include "support.h"
+
+#include "dt.h"
+
+void const *Dt::_fdt;
+
+void Dt::init(unsigned long fdt_addr)
+{
+  if (!fdt_addr)
+    return;
+
+  _fdt = (void const *)fdt_addr;
+
+  int fdt_check = fdt_check_header(_fdt);
+  if (fdt_check < 0)
+    {
+      _fdt = nullptr;
+      warn("FDT sanity check failed: %s (%d)\n",
+           fdt_strerror(fdt_check), fdt_check);
+      return;
+    }
+
+    info("FDT available (size=%d)\n", fdt_totalsize(_fdt));
+}
+
+void Dt::check_for_dt()
+{
+  if (!_fdt)
+    panic("This platform needs a device tree, please provide one.");
+}
+
+Dt::Node Dt::node_by_path(char const *path)
+{
+  return Node(fdt_path_offset(_fdt, path));
+}
+
+Dt::Node Dt::node_by_phandle(uint32_t phandle)
+{
+  return Node(fdt_node_offset_by_phandle(_fdt, phandle));
+}
+
+Dt::Node Dt::node_by_compatible(char const *path)
+{
+  return Node(fdt_node_offset_by_compatible(_fdt, -1, path));
+}
+
+char const *Dt::Node::get_prop_str(char const *name) const
+{
+  int len;
+  char const *str = get_prop<char>(name, &len);
+  // Ensure that the string is null-terminated as required by DTB spec.
+  if (str && strnlen(str, len) == static_cast<size_t>(len))
+    {
+      warn("Property '%s' does not hold a valid string.", name);
+      return nullptr;
+    }
+  return str;
+}
+
+bool Dt::Node::stringlist_search(char const *name, char const *value) const
+{
+  return fdt_stringlist_search(_fdt, _off, name, value) >= 0;
+}
+
+bool Dt::Node::get_addr_size_cells(unsigned &addr_cells,
+                                   unsigned &size_cells) const
+{
+  int addr = fdt_address_cells(_fdt, _off);
+  int size = fdt_size_cells(_fdt, _off);
+  if (addr < 0 || addr > 2 || size < 0 || size > 2)
+    {
+      warn("Unsupported value for address/size cells: %d/%d\n", addr, size);
+      return false;
+    }
+  addr_cells = addr;
+  size_cells = size;
+  return true;
+}
+
+bool Dt::Node::translate_reg(Node parent,
+                             l4_uint64_t &addr, l4_uint64_t const &size) const
+{
+  if (parent.is_root_node())
+    return true;
+
+  unsigned addr_cells, size_cells;
+  if (!parent.get_addr_size_cells(addr_cells, size_cells))
+    return false;
+
+  Node parent_parent = parent.parent_node();
+  unsigned parent_addr_cells, parent_size_cells;
+  if (!parent_parent.get_addr_size_cells(parent_addr_cells, parent_size_cells))
+    return false;
+
+  auto ranges = parent.get_prop_array("ranges", {addr_cells, parent_addr_cells,
+                                                 size_cells});
+
+  // If no ranges property is present, there exists no mapping between parent
+  // and child address space.
+  if (!ranges.is_present())
+    return false; // no translation possible
+
+  if (!ranges.is_valid())
+  {
+    parent.warn("Unexpected ranges property size.\n");
+    return false;
+  }
+
+  // Empty ranges property?
+  if (ranges.elements() == 0)
+    return true; // identity mapping
+
+  // Iterate over all the address space mappings
+  for (unsigned i = 0; i < ranges.elements(); i++)
+    {
+      Range range{ranges.get(i, 0), ranges.get(i, 1), ranges.get(i, 2)};
+      if (range.translate(addr, size))
+        return translate_reg(parent_parent, addr, size);
+    }
+  return false;
+}
+
+bool Dt::Node::get_reg_array(Node parent, Reg_array_prop &regs) const
+{
+  if (!parent.is_valid())
+    return false;
+
+  unsigned addr_cells, size_cells;
+  if (!parent.get_addr_size_cells(addr_cells, size_cells))
+    return false;
+
+  regs = get_prop_array("reg", {addr_cells, size_cells});
+  return regs.is_valid();
+}
+
+bool Dt::Node::get_reg_val(Node parent, Reg_array_prop const &regs,
+                           unsigned index, l4_uint64_t *out_addr,
+                           l4_uint64_t *out_size) const
+{
+  if (index >= regs.elements())
+    return false;
+
+  l4_uint64_t addr = regs.get(index, 0);
+  l4_uint64_t size = regs.get(index, 1);
+  if (!translate_reg(parent, addr, size))
+    return false;
+
+  if (out_addr)
+    *out_addr = addr;
+  if (out_size)
+    *out_size = size;
+  return true;
+}
+
+
+void Dt::setup_memory()
+{
+  // Iterate all memory nodes
+  nodes_by_prop_value("device_type", "memory", 7, [](Dt::Node mem)
+    {
+      mem.for_each_reg([](l4_uint64_t start, l4_uint64_t sz)
+        {
+          info("Add memory to RAM: %lluMiB (%10llx - %10llx)\n",
+                 sz >> 20, start, start + sz - 1);
+
+          mem_manager->ram->add(Region::start_size(start, sz, ".ram",
+                                                   Region::Ram));
+        });
+    });
+
+  // Scan reserved memory regions
+  Node rsrv_mem = node_by_path("/reserved-memory");
+  if (rsrv_mem.is_valid())
+    {
+      info("Reserved memory areas:\n");
+      rsrv_mem.for_each_subnode([](Dt::Node rsrv)
+        {
+          char const *name = rsrv.get_name(".reserved");
+          rsrv.for_each_reg([=](l4_uint64_t start, l4_uint64_t sz)
+            {
+              info("  %20s: %10llx - %10llx\n", name, start, start + sz - 1);
+
+              mem_manager->regions->add(
+                Region::start_size(start, sz, name, Region::Arch));
+            });
+        });
+    }
+
+  // Add device tree to memory map
+  mem_manager->regions->add(
+    Region::n(_fdt, static_cast<l4_uint8_t const *>(_fdt) + fdt_totalsize(_fdt),
+              ".dtb", Region::Arch));
+}
+
+void Dt::dump()
+{
+  auto size = fdt_totalsize(_fdt);
+  printf("DT: Dumping device tree:\n");
+  for (auto i = 0u; i < size; i++)
+    printf("%02x ", static_cast<l4_uint8_t const *>(_fdt)[i]);
+  printf("\n");
+}
---
+++ server/src/dt.h
@@ -0,0 +1,383 @@
+/* SPDX-License-Identifier: GPL-2.0-only or License-Ref-kk-custom */
+/*
+ * Copyright (C) 2021 Kernkonzept GmbH.
+ * Author(s): Georg Kotheimer <georg.kotheimer@kernkonzept.com>
+ */
+
+#pragma once
+
+#include <assert.h>
+#include <stdio.h>
+
+#include <l4/cxx/type_traits>
+#include <l4/sys/l4int.h>
+
+extern "C" {
+#include <libfdt.h>
+}
+
+/**
+ * Read-only wrapper around a flattened device tree.
+ */
+class Dt
+{
+public:
+  enum { Verbose_dt = 0 };
+
+  /**
+   * Optional return value to control callback-driven loops.
+   */
+  enum Cb
+  {
+    Break,
+    Continue,
+  };
+
+  /**
+   * Accessor for array properties with regularly sized cells.
+   */
+  template<unsigned N>
+  class Array_prop
+  {
+  public:
+    Array_prop()
+      : _prop(nullptr), _len(0)
+    {}
+
+    Array_prop(fdt32_t const *prop, unsigned len, unsigned const cell_size[N])
+      : _prop(prop), _len(len)
+    {
+      _elem_size = 0;
+      for (unsigned i = 0; i < N; i++)
+        {
+          _cell_size[i] = cell_size[i];
+          _elem_size += cell_size[i];
+        }
+    }
+
+    bool is_present() const
+    { return _prop != nullptr; }
+
+    bool is_valid() const
+    { return is_present() && (_len % _elem_size) == 0; }
+
+    template<typename T = l4_uint64_t>
+    T get(unsigned element, unsigned cell) const
+    {
+      assert(cell < N);
+      assert(element < elements());
+
+      unsigned off = element * _elem_size;
+      for (unsigned i = 0; i < cell; i++)
+        off += _cell_size[i];
+
+      return read_value<T>(_prop + off, _cell_size[cell]);
+    }
+
+    unsigned elements() const
+    { return _len / _elem_size; }
+
+  private:
+    fdt32_t const *_prop;
+    unsigned _len;
+    unsigned _cell_size[N];
+    unsigned _elem_size;
+  };
+
+  /**
+   * Data and methods associated with a range property in a device tree
+   *
+   * Ranges in a device tree describe the translation of regions from one
+   * domain to another.
+   */
+  class Range
+  {
+  public:
+    using Cell = l4_uint64_t;
+
+    /**
+     * Translate an address from one domain to another
+     *
+     * This function takes an address and a size and translates the address
+     * from one domain to another if there is a matching range.
+     *
+     * \param[inout]  address    Address that shall be translated
+     * \param[in]     size Size  Size associated with the address
+     */
+    bool translate(Cell &address, Cell const &size)
+    {
+      if (match(address, size))
+        {
+          address = (address - _child) + _parent;
+          return true;
+        }
+      return false;
+    }
+
+    Range(Cell const &child, Cell const &parent, Cell const &length)
+    : _child{child}, _parent{parent}, _length{length} {};
+
+  private:
+    // Child bus address
+    Cell _child;
+    // Parent bus address
+    Cell _parent;
+    // Range size
+    Cell _length;
+
+    // [address, address + size] subset of [child, child + length] ?
+    bool match(Cell const &address, Cell const &size) const
+    {
+      auto address_max = address + size;
+      auto child_max = _child + _length;
+      return (_child <= address) && (address_max <= child_max);
+    }
+  };
+
+  class Node
+  {
+  public:
+    Node() : _off(-1) {}
+    explicit Node(int off) : _off(off) {}
+
+    bool is_valid() const
+    { return _off >= 0; }
+
+    bool is_root_node() const
+    { return _off == 0; }
+
+    Node parent_node() const
+    { return Node(fdt_parent_offset(_fdt, _off)); }
+
+    char const *get_name(char const *default_name = nullptr) const
+    {
+      if (is_root_node())
+        return "/";
+
+      auto name = fdt_get_name(_fdt, _off, nullptr);
+      return name ? name : default_name;
+    }
+
+    bool has_prop(char const *name) const
+    {
+      return fdt_getprop_namelen(_fdt, _off, name, strlen(name), nullptr);
+    }
+
+    template<typename R>
+    R const *get_prop(char const *name, int *size = nullptr) const
+    {
+      auto prop = static_cast<R const *>(
+        fdt_getprop_namelen(_fdt, _off, name, strlen(name), size));
+
+      if (prop && size)
+        *size /= sizeof(R);
+
+      return prop;
+    }
+
+    template<typename T>
+    bool get_prop_val(char const *name, T &val) const
+    {
+      int len;
+      auto prop = get_prop<fdt32_t>(name, &len);
+      if (!prop)
+        return false;
+
+      val = read_value<T>(prop, len);
+      return true;
+    }
+
+    bool get_prop_u32(char const *name, l4_uint32_t &val) const
+    { return get_prop_val(name, val); }
+
+    bool get_prop_u64(char const *name, l4_uint64_t &val) const
+    { return get_prop_val(name, val); }
+
+    char const *get_prop_str(char const *name) const;
+
+    template<unsigned N>
+    Array_prop<N> get_prop_array(char const *name,
+                                 unsigned const (&elems)[N]) const
+    {
+      int len;
+      const fdt32_t *cells = get_prop<fdt32_t>(name, &len);
+      if (cells)
+        return Array_prop<N>(cells, len, elems);
+      else
+        return Array_prop<N>();
+    }
+
+    bool stringlist_search(char const *name, char const *value) const;
+
+    bool get_addr_size_cells(unsigned &addr_cells, unsigned &size_cells) const;
+
+    template<typename CB>
+    void for_each_reg(CB &&cb) const
+    {
+      Node parent = parent_node();
+      Reg_array_prop regs;
+      if (!get_reg_array(parent, regs))
+        return;
+
+      for (unsigned i = 0; i < regs.elements(); i++)
+        {
+          l4_uint64_t addr, size;
+          if (get_reg_val(parent, regs, i, &addr, &size))
+            if (invoke_cb(cb, addr, size) == Break)
+              break;
+        }
+    }
+
+    bool get_reg(unsigned index, l4_uint64_t *addr,
+                 l4_uint64_t *size = nullptr) const
+    {
+      Node parent = parent_node();
+      Reg_array_prop regs;
+      if (!get_reg_array(parent, regs))
+        return false;
+
+      return get_reg_val(parent, regs, index, addr, size);
+    }
+
+    bool check_compatible(char const *compatible) const
+    { return fdt_node_check_compatible(_fdt, _off, compatible) == 0; }
+
+    bool check_device_type(char const *device_type) const
+    {
+      char const *prop = get_prop_str("device_type");
+      return prop && !strcmp(prop, device_type);
+    }
+
+    bool is_enabled() const
+    {
+      char const *status = get_prop_str("status");
+      return !status || !strcmp("ok", status) || !strcmp("okay", status);
+    }
+
+    template<typename CB>
+    void for_each_subnode(CB &&cb) const
+    {
+      int node;
+      fdt_for_each_subnode(node, _fdt, _off)
+        if (invoke_cb(cb, Node(node)) == Break)
+          return;
+    }
+
+    template<typename... Args>
+    void warn(char const *format, Args &&... args) const
+    {
+      if (auto name = get_name())
+        printf("DT[%s]: ", name);
+      else
+        printf("DT[@%d]: ", _off);
+
+      printf(format, cxx::forward<Args>(args)...);
+    }
+
+    template<typename... Args>
+    void info(char const *format, Args &&... args) const
+    {
+      if (Verbose_dt)
+        warn(format, cxx::forward<Args>(args)...);
+    }
+
+  private:
+    using Reg_array_prop = Array_prop<2>;
+
+    bool translate_reg(Node parent,
+                       l4_uint64_t &addr, l4_uint64_t const &size) const;
+
+    bool get_reg_array(Node parent, Reg_array_prop &regs) const;
+
+    bool get_reg_val(Node parent, Reg_array_prop const &regs, unsigned index,
+                     l4_uint64_t *out_addr, l4_uint64_t *out_size) const;
+
+    int _off;
+  };
+
+  static void init(unsigned long fdt_addr);
+  static void check_for_dt();
+
+  static Node node_by_path(char const *path);
+  static Node node_by_phandle(uint32_t phandle);
+  static Node node_by_compatible(char const *compatible);
+
+  template<typename CB, typename NEXT>
+  static void nodes_by(CB &&cb, NEXT &&next)
+  {
+    for (int node = next(-1); node >= 0; node = next(node))
+      if (invoke_cb(cb, Node(node)) == Break)
+        return;
+  }
+
+  template<typename CB>
+  static void nodes_by_prop_value(char const *name, void const *val, int len,
+                                  CB &&cb)
+  {
+    nodes_by(cb, [=](int node)
+      { return fdt_node_offset_by_prop_value(_fdt, node, name, val, len); });
+  }
+
+  template<typename CB>
+  static void nodes_by_compatible(char const *compatible, CB &&cb)
+  {
+    nodes_by(cb, [=](int node)
+      { return fdt_node_offset_by_compatible(_fdt, node, compatible); });
+  }
+
+  template<typename T = l4_uint64_t>
+  static T read_value(fdt32_t const *cells, unsigned size)
+  {
+    l4_uint64_t val = 0;
+    for (unsigned i = 0; i < size; i++)
+      val = (val << 32) | fdt32_to_cpu(cells[i]);
+
+    if (size * sizeof(fdt32_t) > sizeof(T))
+      warn("Cell size %lu larger than provided storage type %lu!\n",
+           size * sizeof(fdt32_t), sizeof(T));
+
+    return static_cast<T>(val);
+  }
+
+  static void setup_memory();
+
+  static void dump();
+
+  template<typename... Args>
+  static void warn(char const *format, Args &&... args)
+  {
+    printf("DT: ");
+    printf(format, cxx::forward<Args>(args)...);
+  }
+
+  template<typename... Args>
+  static void info(char const *format, Args &&... args)
+  {
+    if (Verbose_dt)
+      warn(format, cxx::forward<Args>(args)...);
+  }
+
+protected:
+  template<typename R, typename CB, typename... Args>
+  struct invoke_cb_t
+  {
+    static constexpr Cb _(CB &&cb, Args &&... args)
+    { return cb(args...); }
+  };
+
+  template<typename CB, typename... Args>
+  struct invoke_cb_t<void, CB, Args...>
+  {
+    static constexpr Cb _(CB &&cb, Args &&... args)
+    { return cb(args...), Continue; }
+  };
+
+  template<typename CB, typename... Args>
+  static Cb invoke_cb(CB &&cb, Args &&... args)
+  {
+    return invoke_cb_t<decltype(cb(args...)), CB, Args...>::_(
+      cb, cxx::forward<Args>(args)...);
+  }
+
+  static void const *_fdt;
+};
