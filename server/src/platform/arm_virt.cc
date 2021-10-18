ca3ba9f1b69667570fcfd02ac542003d481dd025
Adam Lackorzynski <adam@l4re.org>
2021-10-18 00:00:00 +0000

    Arm: Use DT for arm_virt platform
    Use the FDT supplied by QEMU for the platform, giving
    the benefit that we can now read out the memory descriptors
    and adapt the size of RAM automatically.
    
    Change-Id: Iee4cfb2961202872365fbc665edc0e2f8ed7c5bf

--- server/src/platform/arm_virt.cc
+++ server/src/platform/arm_virt.cc
@@ -9,10 +9,12 @@
 #include <l4/drivers/uart_pl011.h>
 #include "support.h"
 #include "startup.h"
+#include "dt.h"
 
+extern char _start;
 
 namespace {
-class Platform_arm_virt : public Platform_single_region_ram
+class Platform_arm_virt : public Platform_base, public Boot_modules_image_mode
 {
   bool probe() { return true; }
 
@@ -34,6 +36,28 @@ class Platform_arm_virt : public Platform_single_region_ram
     static L4::Uart_pl011 _uart(kuart.base_baud);
     _uart.startup(&r);
     set_stdio_uart(&_uart);
+
+    /* This is a QEMU special. QEMU copies the FDT to the start of the
+     * RAM but does not provide the address in case the payload is
+     * started as ELF.
+     * Further, Fiasco wants to have this part of the RAM, so copy the FDT
+     * somewhere else. */
+    void *fdt = (void *)RAM_BASE;
+    if (fdt_check_header(fdt) == 0)
+      {
+        unsigned s = fdt_totalsize(fdt);
+        unsigned long dst = l4_trunc_page((unsigned long)&_start - s);
+        memmove((void *)dst, fdt, s);
+        boot_args.r[0] = dst;
+      }
+  }
+
+  Boot_modules *modules() { return this; }
+
+  void setup_memory_map()
+  {
+    Dt::check_for_dt();
+    Dt::setup_memory();
   }
 
   void reboot()
