1459274832a40551b1a6ce72b2ec67a677ab3e83
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2022-01-17 00:00:00 +0000

    arm64: convert to common EFI code
    Change-Id: Iae05ee4092632859f98d64f9aa00ccef6b8e079e

--- server/src/ARCH-arm64/efi.cc
+++ server/src/ARCH-arm64/efi.cc
@@ -3,40 +3,16 @@
  * Copyright (C) 2014-2021 Kernkonzept GmbH.
  * Author(s): Jan Klötzke <jan.kloetzke@kernkonzept.com>
  */
-
-extern "C" {
-#include <efi.h>
-#include <efilib.h>
-}
-
+#include "efi-support.h"
 #include "platform.h"
-#include "support.h"
 #include "startup.h"
-
-extern "C" void armv8_disable_mmu(void);
+#include "support.h"
 
 extern "C" EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *systab);
 EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *systab)
 {
   ctor_init();
-
-  // Bye bye UEFI
-  UINTN num_entries;
-  UINTN key;
-  UINTN desc_size;
-  uint32_t desc_ver;
-  InitializeLib(image, systab);
-  LibMemoryMap(&num_entries, &key, &desc_size, &desc_ver);
-  uefi_call_wrapper(systab->BootServices->ExitBootServices, 2, image, key);
-
-  // UEFI has interrupts enabled as per specification. Fiasco boot protocol
-  // requires interrupts to be disabled, though. Overall, ensure all is
-  // masked.
-  asm volatile("msr DAIFSet, #0xf");
-
-  // UEFI did setup the MMU with caches. Disable everything because we move
-  // code around and will jump to it at the end.
-  armv8_disable_mmu();
+  efi.init(image, systab);
 
   // Do the usual platform iteration as done by head.cc.
   Platform_base::iterate_platforms();
