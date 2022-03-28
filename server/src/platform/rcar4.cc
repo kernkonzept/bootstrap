1458dd48d55931f96043444c26ee024473025e85
Adam Lackorzynski <adam@l4re.org>
2022-03-28 00:00:00 +0000

    Add rcar4 BSP
    Change-Id: Idf30c1924aab8c076236325a03f53c66c78850f2

---
+++ server/src/platform/rcar4.cc
@@ -0,0 +1,53 @@
+/*!
+ * \file
+ * \brief  Support for Renesas Gen4
+ *
+ * \date   2022
+ * \author Adam Lackorzynski <adam@l4re.org>
+ *
+ */
+/*
+ * (c) 2022 Author(s)
+ *
+ * This file is part of L4Re and distributed under the terms of the
+ * GNU General Public License 2.
+ * Please see the COPYING-GPL-2 file for details.
+ */
+
+#include <l4/drivers/uart_sh.h>
+#include "support.h"
+#include "startup.h"
+
+namespace {
+class Platform_arm_rcar4 : public Platform_dt
+{
+  bool probe() { return true; }
+
+  void init()
+  {
+    kuart.base_address = 0xe6e88000;
+    kuart.reg_shift    = 0;
+    kuart.base_baud    = 14745600;
+    kuart.base_address = 0xe6e60000;
+    kuart.irqno        = 283;
+
+    kuart.access_type  = L4_kernel_options::Uart_type_mmio;
+    kuart_flags       |=   L4_kernel_options::F_uart_base
+                         | L4_kernel_options::F_uart_baud
+                         | L4_kernel_options::F_uart_irq;
+    static L4::Uart_sh _uart;
+    static L4::Io_register_block_mmio r(kuart.base_address);
+    _uart.startup(&r);
+    set_stdio_uart(&_uart);
+  }
+
+  Boot_modules *modules() { return this; }
+
+  void reboot()
+  {
+    reboot_psci();
+  }
+};
+}
+
+REGISTER_PLATFORM(Platform_arm_rcar4);
