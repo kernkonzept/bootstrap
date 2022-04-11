c655f3b12c13925e32265536b8ceedb169ea51ee
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2022-04-11 10:58:57 +0200

    arm: add common MPU definitions
    Add EL2 Armv8-R MPU support.
    
    Change-Id: I5b46f6b53fc19b4a5dccb0a91ab04b3aa01e2e7a

---
+++ server/src/arch/arm/mpu_consts.h
@@ -0,0 +1,29 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2022 Kernkonzept GmbH.
+ * Author(s): Jan Klötzke <jan.kloetzke@kernkonzept.com>
+ */
+
+#pragma once
+
+/*
+ * Constants that need are needed by assembler code too.
+ */
+
+/**
+ * Memory Attribute Indirection (MAIR0)
+ * Attr0: Device-nGnRnE memory
+ * Attr1: Normal memory, Inner/Outer Non-cacheable
+ * Attr2: Normal memory, Read-Allocate, no Write-Allocate,
+ *        Inner/Outer Write-Through Cacheable (Non-transient)
+ *
+ * ATTENTION: These must be the same as in Fiasco if the MPU must be left
+ * enabled when jumping to Fiasco!
+ */
+
+#define MAIR0_BITS 0x00aa4400
+#define MAIR1_BITS 0
+
+#define MPU_ATTR_NORMAL     (2 << 1)
+#define MPU_ATTR_DEVICE     (0 << 1)
+#define MPU_ATTR_BUFFERED   (1 << 1)
