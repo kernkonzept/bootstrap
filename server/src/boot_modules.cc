c04cb213600232f1cf2b5fdb2af94db6d5401aa3
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2022-10-03 00:00:00 +0000

    Use _stext symbol to find start of binary
    The _start symbol may have the lower bit set on arm thumb builds which
    confuses the offset calculation. Use the _stext symbol that does not
    suffer from this deficiency because it is defined by the linker and not
    a function.
    
    Change-Id: Id9a0135f36877f0cefcf8d2b25d56c84aa6c2a5d

--- server/src/boot_modules.cc
+++ server/src/boot_modules.cc
@@ -386,8 +386,8 @@ void init_modules_infos()
 {
 #if defined(__PIC__) || defined(__PIE__)
   // Fixup header addresses when being compiled position independent
-  extern int _start;      /* begin of image -- defined in crt0.S */
-  l4_uint64_t off = (l4_uint64_t)&_start - image_info.start_of_binary;
+  extern int _stext;      /* begin of image -- defined in bootstrap.ld.in */
+  l4_uint64_t off = (l4_uint64_t)&_stext - image_info.start_of_binary;
   image_info.start_of_binary   += off;
   image_info.end_of_binary     += off;
   image_info.module_data_start += off;
