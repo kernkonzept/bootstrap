51d3981471f1f55c8e8e8d15f16d872443a1230f
Frank Mehnert <frank.mehnert@kernkonzept.com>
2023-06-05 00:00:00 +0000

    detect Clang defines for selecting memory barriers
    The defines by Clang are different from Gcc. Consider this for selecting
    the proper mb() Assembler code.
    
    See also I1dcb6fbe6d31e3b96a3e4b852b5adb56d93647d6.
    
    Change-Id: I65b69e91faf1c4b3922f7707836e86351a611278

--- server/src/qemu_fw_cfg.cc
+++ server/src/qemu_fw_cfg.cc
@@ -18,9 +18,9 @@ namespace
 {
 
 /// Enforces ordering between MMIO register access and/or shared memory accesses.
-#if defined(__ARM_ARCH_7A__)
+#if defined(__ARM_ARCH) && __ARM_ARCH == 7
 static inline void mb() { asm volatile ("dsb" : : : "memory"); }
-#elif defined(__ARM_ARCH_8A)
+#elif defined(__ARM_ARCH) && __ARM_ARCH >= 8
 // Since ARMv8 the memory model is other-multi-copy atomic, i.e. a memory write
 // observed by an observer is also visible to all other observers. In other
 // words, the memory write is either visible only locally to the originator (for
