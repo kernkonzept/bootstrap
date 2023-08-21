dcd3b2beb6402963a9a46249c6bb469e03ae85ba
Frank Mehnert <frank.mehnert@kernkonzept.com>
2023-08-21 00:00:00 +0000

    fix Clang build error
    Now that "region.h" includes <assert.h>, libc_support+.cc includes
    <assert.h> as well. Hence, the __assert_fail() function signature has to
    match the signature in <assert.h> -- which requires a `throw()` -- or
    rather `noexcept` keyword (the latter is C++11 or newer).
    
    This fixes a Clang build error:
    
      libc_support+.cc:120:1: error:
        '__assert' is missing exception specification 'throw()'
        __assert(const char *assertion, const char * filename,
        ^
    
    Change-Id: Ibdcb664e5046e5d46812fc2e285b915d89700e1a

--- server/src/libc_support+.cc
+++ server/src/libc_support+.cc
@@ -117,11 +117,8 @@ void exit(int c) throw()
 void (*__exit_cleanup) (int) = 0;
 
 extern "C" void __attribute__((noreturn))
-__assert(const char *, const char *, unsigned int, const char *);
-
-extern "C" void __attribute__((noreturn))
 __assert(const char *assertion, const char * filename,
-         unsigned int linenumber, const char * function)
+         unsigned int linenumber, const char * function) noexcept
 {
   printf("%s:%d: %s: Assertion `%s' failed.\n",
 				filename,
