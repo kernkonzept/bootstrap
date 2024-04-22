e4d23fe50508d47c82925dba6aa36816f129a170
Stephan Gerhold <stephan.gerhold@kernkonzept.com>
2024-04-22 00:00:00 +0000

    efi: Move setup_gop() to common EFI code
    The functionality inside x86_efi_pc that reads the framebuffer
    information using the EFI Graphics Output Protocol (GOP) is actually
    independent of x86 and also works on Arm platforms with EFI. Move it to
    efi-support like all other common EFI code so Arm platforms can also use
    this functionality.
    
    Change-Id: I4b23ad594e4817b5ef4c8ca8f951b24480b4195a

--- server/src/efi-support.cc
+++ server/src/efi-support.cc
@@ -6,6 +6,8 @@
  */
 
 #include <l4/drivers/uart_base.h>
+#include <l4/sys/types.h>
+#include <l4/util/mb_info.h>
 #include "efi-support.h"
 #include "panic.h"
 
@@ -206,6 +208,186 @@ Efi::setup_memory()
 
   // merge adjacent regions
   ram->optimize();
+}
+
+l4util_l4mod_info *Efi::construct_mbi(l4util_l4mod_info *mbi)
+{
+  // we use the copy here instead of reading it out directly because
+  // EFI is already exited in setup_memory_map which is called much too
+  // early
+
+  unsigned long long vbe_sz = sizeof(l4util_mb_vbe_ctrl_t) + sizeof(l4util_mb_vbe_mode_t);
+  l4_addr_t vbe_addr = mem_manager->find_free_ram(vbe_sz);
+  if (!vbe_addr)
+    {
+      printf("EFI: Unable to reserve region to hold VBE config.\n");
+      return mbi;
+    }
+
+  mem_manager->regions->add(Region::start_size(vbe_addr, vbe_sz, ".vbe",
+                                               Region::Root, L4_FPAGE_RW));
+
+  mbi->vbe_ctrl_info = vbe_addr;
+  mbi->vbe_mode_info = vbe_addr + sizeof(l4util_mb_vbe_ctrl_t);
+
+  l4util_mb_vbe_ctrl_t *vbe = (l4util_mb_vbe_ctrl_t *)(l4_addr_t)mbi->vbe_ctrl_info;
+  l4util_mb_vbe_mode_t *vbi = (l4util_mb_vbe_mode_t *)(l4_addr_t)mbi->vbe_mode_info;
+
+  // Use the structures phys_base plus the following reserved1 field to
+  // hold a 64-bit value
+  vbi->phys_base    = _video_fb_phys_base;
+  vbi->reserved1    = _video_fb_phys_base >> 32;
+  vbi->x_resolution = _video_info.HorizontalResolution;
+  vbi->y_resolution = _video_info.VerticalResolution;
+
+  vbe->total_memory = (_video_fb_size + 64 * 1024 - 1) / (64 * 1024); // in 64k chunks
+
+  switch (_video_info.PixelFormat)
+    {
+    case PixelRedGreenBlueReserved8BitPerColor:
+      vbi->red_mask_size = 8;
+      vbi->green_mask_size = 8;
+      vbi->blue_mask_size = 8;
+
+      vbi->red_field_position = 0;
+      vbi->green_field_position = 8;
+      vbi->blue_field_position = 16;
+      break;
+
+    case PixelBlueGreenRedReserved8BitPerColor:
+      vbi->red_mask_size = 8;
+      vbi->green_mask_size = 8;
+      vbi->blue_mask_size = 8;
+
+      vbi->red_field_position = 16;
+      vbi->green_field_position = 8;
+      vbi->blue_field_position = 0;
+      break;
+
+    case PixelBitMask:
+
+      vbi->red_mask_size
+        = __builtin_popcount(_video_info.PixelInformation.RedMask);
+      vbi->green_mask_size
+        = __builtin_popcount(_video_info.PixelInformation.GreenMask);
+      vbi->blue_mask_size
+        = __builtin_popcount(_video_info.PixelInformation.BlueMask);
+
+      vbi->red_field_position = ffs(_video_info.PixelInformation.RedMask);
+      if (vbi->red_field_position)
+        --vbi->red_field_position;
+
+      vbi->green_field_position = ffs(_video_info.PixelInformation.GreenMask);
+      if (vbi->green_field_position)
+        --vbi->green_field_position;
+
+      vbi->blue_field_position = ffs(_video_info.PixelInformation.BlueMask);
+      if (vbi->blue_field_position)
+        --vbi->blue_field_position;
+      break;
+
+    default:
+    case PixelBltOnly:
+      return mbi;
+    }
+
+  // All format defaults:
+  vbi->bytes_per_scanline = _video_info.PixelsPerScanLine * 4;
+  vbi->bits_per_pixel = 32;
+
+  mbi->flags |= L4UTIL_MB_VIDEO_INFO;
+
+  return mbi;
+}
+
+void Efi::setup_gop()
+{
+  EFI_GRAPHICS_OUTPUT_PROTOCOL *gop;
+  EFI_GUID efi_gop = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID;
+
+  EFI_STATUS rc = LibLocateProtocol(&efi_gop, (void **)&gop);
+  if (EFI_ERROR(rc))
+    {
+      Print((CHAR16 *)L"No Graphics\n");
+      return;
+    }
+
+  int i, imax;
+  void *r;
+
+  imax = gop->Mode->MaxMode;
+
+  const bool verbose = false;
+
+  if (verbose)
+    Print((CHAR16 *)L"GOP reports MaxMode %d\n", imax);
+  for (i = 0; i < imax; i++)
+    {
+      EFI_GRAPHICS_OUTPUT_MODE_INFORMATION *info;
+      UINTN SizeOfInfo;
+      r = uefi_call_wrapper((void *)gop->QueryMode, 4, gop, i, &SizeOfInfo,
+                             &info);
+      if (EFI_ERROR(r) && (EFI_STATUS)r == EFI_NOT_STARTED)
+        {
+          r = uefi_call_wrapper((void *)gop->SetMode, 2, gop,
+                                gop->Mode->Mode);
+          // TODO: check r
+          r = uefi_call_wrapper((void *)gop->QueryMode, 4, gop, i,
+                                &SizeOfInfo, &info);
+        }
+
+      if (EFI_ERROR(r))
+        {
+          CHAR16 Buffer[64];
+          StatusToString(Buffer, (EFI_STATUS)r);
+          Print((CHAR16 *)L"EFI GOP: %d: Bad response from QueryMode: %s (%d)\n",
+                i, Buffer, r);
+          continue;
+        }
+
+      if (verbose)
+        {
+          Print((CHAR16 *)L"%c%d: %dx%d ",
+                memcmp(info, gop->Mode->Info, sizeof(*info)) == 0 ? '*' : ' ',
+                i,
+                info->HorizontalResolution,
+                info->VerticalResolution);
+          switch (info->PixelFormat)
+            {
+            case PixelRedGreenBlueReserved8BitPerColor:
+                Print((CHAR16 *)L"RGBR");
+              break;
+            case PixelBlueGreenRedReserved8BitPerColor:
+                Print((CHAR16 *)L"BGRR");
+              break;
+            case PixelBitMask:
+              Print((CHAR16 *)L"R:%08x G:%08x B:%08x X:%08x",
+                    info->PixelInformation.RedMask,
+                    info->PixelInformation.GreenMask,
+                    info->PixelInformation.BlueMask,
+                    info->PixelInformation.ReservedMask);
+              break;
+            case PixelBltOnly:
+              Print((CHAR16 *)L"(blt only)");
+              break;
+            default:
+              Print((CHAR16 *)L"(Invalid pixel format)");
+              break;
+            }
+          Print((CHAR16 *)L" pitch %d\n", info->PixelsPerScanLine);
+        }
+
+      if (0 == memcmp(info, gop->Mode->Info, sizeof(*info)))
+        {
+          _video_info         = *info;
+          _video_fb_phys_base = gop->Mode->FrameBufferBase;
+          _video_fb_size      = gop->Mode->FrameBufferSize;
+
+          if (verbose)
+            Print((CHAR16 *)L" base=%x  size=%x\n",
+                  _video_fb_phys_base, _video_fb_size);
+        }
+    }
 }
 
 Efi efi;
--- server/src/efi-support.h
+++ server/src/efi-support.h
@@ -17,6 +17,8 @@ class Efi
 public:
   void init(EFI_HANDLE image, EFI_SYSTEM_TABLE *systab);
   void setup_memory();
+  void setup_gop();
+  l4util_l4mod_info *construct_mbi(l4util_l4mod_info *mbi);
 
   void exit_boot_services();
 
@@ -31,6 +33,10 @@ private:
   EFI_HANDLE _image;
   EFI_SYSTEM_TABLE *_sys_table;
   void *_acpi_rsdp;
+
+  EFI_GRAPHICS_OUTPUT_MODE_INFORMATION _video_info;
+  EFI_PHYSICAL_ADDRESS                 _video_fb_phys_base;
+  unsigned                             _video_fb_size;
 };
 
 extern Efi efi;
