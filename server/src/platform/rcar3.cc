0ea1b28683ddd157ea6dda55c17995faa513319c
Adam Lackorzynski <adam@l4re.org>
2025-12-29 00:00:00 +0000

    arm/rcar3: Convert to DT platform
    Convert to a Device Tree platform to avoid specifying memory regions
    that might differ in specific rcar3 variants.
    
    Change-Id: I01e25dd1244840626e35047d0b586d1c0706dd6b

--- server/src/platform/rcar3.cc
+++ server/src/platform/rcar3.cc
@@ -15,11 +15,10 @@
 #include <l4/drivers/uart_sh.h>
 #include "support.h"
 #include "startup.h"
-#include "platform-arm.h"
+#include "platform_dt-arm.h"
 
 namespace {
-class Platform_arm_rcar3 : public Platform_arm,
-                           public Boot_modules_image_mode
+class Platform_arm_rcar3 : public Platform_dt_arm
 {
   bool probe() override { return true; }
 
@@ -38,42 +37,6 @@ class Platform_arm_rcar3 : public Platform_arm,
     static L4::Io_register_block_mmio r(kuart.base_address);
     _uart.startup(&r);
     set_stdio_uart(&_uart);
-  }
-
-  Boot_modules *modules() override { return this; }
-
-  void setup_memory_map() override
-  {
-    // product register for R-Car Gen3
-    l4_uint32_t prr = *(l4_uint32_t*)0xFFF00044;
-    l4_uint32_t ufamily = (prr & 0xff00) >> 8;
-    const char *sfamily = NULL;
-    mem_manager->ram->add(Region(RAM_BASE,    0x07fffffff, ".ram", Region::Ram));
-    mem_manager->ram->add(Region(0x600000000, 0x63fffffff, ".ram", Region::Ram));
-    switch (ufamily)
-      {
-      case 0x4f: sfamily = "r8a7795"; break;
-      case 0x52: sfamily = "r8a7796"; break;
-      }
-    if (sfamily)
-      {
-        char rev[16];
-        if ((prr & 0x7fff) == 0x5210)
-          snprintf(rev, sizeof(rev), "ES1.1");
-        else
-          snprintf(rev, sizeof(rev), "ES%u.%u",
-                   ((prr >> 4) & 0x0f) + 1, prr & 0xf);
-
-        if (ufamily == 0x4f)
-          {
-            mem_manager->ram->add(Region(0x500000000, 0x53fffffff, ".ram", Region::Ram));
-            mem_manager->ram->add(Region(0x700000000, 0x73fffffff, ".ram", Region::Ram));
-          }
-
-        printf("  Found R-Car Gen3 %s %s\n", sfamily, rev);
-      }
-    else
-      printf("Configured for R-Car Gen3 but found unknown product (prr=0x%08x)\n", prr);
   }
 
   void reboot() override
