56c411fd4a0e89b5ef3dd1326d15223374ccf6ae
Valentin Gehrke <valentin.gehrke@kernkonzept.com>
2026-02-16 00:00:00 +0000

    arch/arm/mem_v6.h: Add missing semicolons to dsb_system calls
    Calls were added in I0c1e8a68e7c4b7b66f67317a6afd8f35538bfe49, but
    without semicolons
    
    Change-Id: Id3b6df31c3538d717c50ec2939e83cf49affcee1
    Signed-off-by: Valentin Gehrke <valentin.gehrke@kernkonzept.com>

--- server/src/arch/arm/mem_v6.h
+++ server/src/arch/arm/mem_v6.h
@@ -3,13 +3,13 @@
 void Cache::Data::clean()
 {
   asm volatile("mcr p15, 0, %0, c7, c10, 0" : : "r" (0) : "memory");
-  Barrier::dsb_system()
+  Barrier::dsb_system();
 }
 
 void Cache::Data::clean(unsigned long addr)
 {
   asm volatile("mcr p15, 0, %0, c7, c10, 1" : : "r" (addr) : "memory");
-  Barrier::dsb_system()
+  Barrier::dsb_system();
 }
 
 void Cache::Data::clean(unsigned long start, unsigned long size)
@@ -26,13 +26,13 @@ void Cache::Data::clean(unsigned long start, unsigned long size)
 void Cache::Data::flush(unsigned long addr)
 {
   asm volatile("mcr p15, 0, %0, c7, c14, 1" : : "r" (addr) : "memory");
-  Barrier::dsb_system()
+  Barrier::dsb_system();
 }
 
 void Cache::Data::inv(unsigned long addr)
 {
   asm volatile("mcr p15, 0, %0, c7, c6, 1" : : "r" (addr) : "memory");
-  Barrier::dsb_system()
+  Barrier::dsb_system();
 }
 
 bool Cache::Data::enabled()
