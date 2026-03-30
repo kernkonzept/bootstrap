da5a1598cfb58069641a4f0c5b6a38bce61e05bc
Adam Lackorzynski <adam@l4re.org>
2026-03-30 00:00:00 +0000

    arm/lx2160: Use arm_gen BSP
    Functionality for lx2160 platforms is covered by arm_gen BSP, so
    just use this one. Add "fsl,qoriq-mc"-specific quirk to arm_gen.
    
    Change-Id: I8de9f402e0423957cc41cb445b5dcb76f7f8bf38
    Signed-off-by: Adam Lackorzynski <adam@l4re.org>

--- server/src/platform/arm_gen.cc
+++ server/src/platform/arm_gen.cc
@@ -47,6 +47,9 @@ class Platform_arm_gen : public Platform_dt_arm
   void late_setup(l4_kernel_info_t *kip) override
   {
     set_dtb_in_kip(kip);
+
+    // We need to start an iterator here over all "drivers" we know and call.
+    setup_plat_lx2160a();
   }
 
   void reboot() override
@@ -61,6 +64,44 @@ class Platform_arm_gen : public Platform_dt_arm
         Dt::Node pshold = dt.node_by_compatible("qcom,pshold");
         if (pshold.is_valid() && pshold.get_reg(0, &addr))
           *reinterpret_cast<volatile l4_uint32_t *>(addr) = 0;
+      }
+  }
+
+  // NXP LX2160
+  static l4_uint64_t read_a72_cpuactlr_el1()
+  {
+    l4_umword_t v;
+    if constexpr (sizeof(long) == 4)
+      asm volatile ("mrrc p15, 0, %Q0, %R0, c15" : "=r"(v));
+    else
+      asm volatile ("mrs %0, S3_1_c15_c2_0" : "=r"(v));
+    return v;
+  }
+
+  // Checking for "fsl,qoriq-mc"
+  void setup_plat_lx2160a()
+  {
+    // Pause the DPAA2 management complex (MC) firmware to avoid faults once
+    // Fiasco enables the IOMMU. If then later someone wants to use the MC, they
+    // have to resume it.
+    enum
+    {
+      MC_GCR1 = 0x00, // General Control Register 1
+
+      MC_GCR1_P1_STOP = 1UL << 31, // Processor 1 Stop
+      MC_GCR1_P2_STOP = 1UL << 30, // Processor 2 Stop
+    };
+
+    Dt::Node mc = dt.node_by_compatible("fsl,qoriq-mc");
+    l4_uint64_t mc_addr;
+    if (mc.is_valid() && mc.get_reg(1, &mc_addr))
+      {
+        L4::Io_register_block_mmio mc_reg(mc_addr);
+        mc_reg.set<l4_uint32_t>(MC_GCR1, MC_GCR1_P1_STOP | MC_GCR1_P2_STOP);
+        printf("  Paused DPAA2 management complex.\n");
+
+        if (!(read_a72_cpuactlr_el1() & (1ULL << 31)))
+          printf("  WARNING: CPUACTLR_EL1[31] is not set -- see corresponding LX2160 erratum!\n");
       }
   }
 };
